a235778b7cb31458d3e0f81cc6e0006a82295adc
Jose Castro <jsalvadorca90@gmail.com>
2025-06-20 01:24:46 -0500

    feat: [11] Modificacion variable tittle, mostrar html.

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'app-angular';
+  protected titulo = 'Hola mundo desde Angular';
 }
