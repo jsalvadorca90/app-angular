0e69bff263e2250fef9cff16017a6d2fa2201f56
Jose Castro <jsalvadorca90@gmail.com>
2025-06-23 17:37:49 -0500

    feat: [18] Relacion entre componentes.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { ComponenteEnLinea } from './componente-en-linea/componente-en-linea';
 import { Interpolacion } from './interpolacion/interpolacion';
 import { NuevoComponente } from './nuevo-componente/nuevo-componente';
+import { Padre } from './padre/padre';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NuevoComponente, ComponenteEnLinea, Interpolacion],
+  imports: [
+    RouterOutlet,
+    NuevoComponente,
+    ComponenteEnLinea,
+    Interpolacion,
+    Padre,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected titulo = 'Hola mundo desde Angular';
+  protected titulo = 'Universidad Angular';
 }
